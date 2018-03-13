563f967ec0a4320dc5ce82c8acb1e9a2eaf9059a
draxvel <ntv3331998@gmail.com>
2018-03-13 14:23:22 +0200

    added order posts

--- app/src/main/java/com/draxvel/simpleblog/ui/main/home/HomePresenter.java
+++ app/src/main/java/com/draxvel/simpleblog/ui/main/home/HomePresenter.java
@@ -8,6 +8,7 @@ import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -39,7 +40,9 @@ public class HomePresenter {
 
      public void setData(){
 
-         firebaseFirestore.collection("Posts").addSnapshotListener(new EventListener<QuerySnapshot>() {
+         Query firstQuery = firebaseFirestore.collection("Posts").orderBy("timestamp", Query.Direction.DESCENDING);
+
+         firstQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
              @Override
              public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
 
@@ -62,6 +65,4 @@ public class HomePresenter {
              }
          });
      }
-
-
 }
