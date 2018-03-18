39e52baeaf246f35d4a96473f6d3f0de863494dc
draxvel <ntv3331998@gmail.com>
2018-03-18 17:47:59 +0200

    refactored signIn method

--- app/src/main/java/com/draxvel/simpleblog/data/IAuth.java
+++ app/src/main/java/com/draxvel/simpleblog/data/IAuth.java
@@ -16,7 +16,7 @@ public interface IAuth {
         void onFailure(final String msg);
     }
 
-    void signIn (String email, String password, SignInCallback callback);
+    void signIn (final String email, final String password, SignInCallback callback);
 
     void signUp (final String email, final String password, SignUpCallback callback);
 }
